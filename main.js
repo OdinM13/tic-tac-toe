f59c72139a1841405753566fa018b89bbc6d5722
OdinM13 <markodinius@gmail.com>
2026-01-31 22:16:37 +0100

    Build function to check for win

--- main.js
+++ main.js
@@ -4,11 +4,58 @@ const Gameboard = (function() {
         [null, null, null],
         [null, null, null]
     ] 
+    const checkRow = (y, marker) => {
+        return board[y].every(val => val === marker);
+    }
+
+    const checkColumn = (x, marker) => {
+        for (let i = 0; i < board.length; i++) {
+            if (board[i][x] !== marker){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    const checkWin = (marker, y, x) => {
+        if (checkRow(y, marker) || checkColumn(x, marker) || checkDiag(marker)) {
+            return true;
+        }
+        return false;
+    }
+    
+    const checkDiag = (marker) => {
+        let boardLength = board.length;
+// Top-Left to Bottom-Right: (0,0, 1,1, 2,2)
+
+        let diag1Win = true;
+        for (let i = 0; i < boardLength; i++) {
+            if (board[i][i] !== marker) {
+                diag1Win = false;
+            }
+        }
+        if (diag1Win) return true;
+
+// Top Right to Bottom-Left: (0,2, 1,1, 2,0) 
+        let diag2Win = true;
+        for (let i = 0; i < boardLength; i++) {
+            if (board[i][boardLength - i - 1] !== marker) {
+                daig2Win = false;
+            }
+        }
+        return diag2Win;
+    }
+    
+    const makeMove = (marker, y, x) => {
+        // Update code. checkWin should only come, if the marker was placed. Replace ternary with solid if-statemenet
+        board[y][x] === null ? board[y][x] = marker : console.log("Place already taken. Choose another spot");
+        // Implement Code to handle the return.
+        checkWin (marker, y, x);
+    }
 
     return {
-        set: (marker, y, x) => board[y][x] === null ? board[y][x] = marker : console.log("Place already taken. Choose another spot"),
+        set: makeMove,
         show: () => console.table(board),
-        eval: (marker) => console.log(board[0].reduce((concat, val) => concat + val, '') === `${marker}${marker}${marker}` ? "Congratulations you won" : "Next Player")
     };
 })();
 
